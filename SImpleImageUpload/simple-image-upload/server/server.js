76297ce365eb64535d26b9af4af911b059620b27
Jaleesa McCrea <jmccrea01@gmail.com>
2019-01-18 04:43:08 -0500

    some server

---
+++ SImpleImageUpload/simple-image-upload/server/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
+
+const app = express()
